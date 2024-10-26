fbb42560aae2e51007dc06315a2351e072de8813
sangcrazy4 <sang_sakarin@outlook.com>
2024-10-26 15:55:05 +0700

    feat: support api v3

--- bitkub/request.py
+++ bitkub/request.py
@@ -1,5 +1,11 @@
 import requests
 
 
-def basic_request(method, url, headers={}, payload={}):
+def basic_request(method, url, headers=None, payload=None):
+    if headers is None:
+        headers = {}
+
+    if payload is None:
+        payload = {}
+
     return requests.request(method, url, headers=headers, data=payload).json()
